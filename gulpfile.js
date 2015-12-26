26f1478ff74b534489f83821111cd3f254190c74
Mike Lewis <mike@Mikes-MacBook-Pro.local>
2015-12-26 12:35:24 -0600

    less

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 	livereload = require('gulp-livereload'),
 	open = require('open'),
 	express = require('express'),
-	port = parseInt(process.argv[2]) || 80;  // not sure why this doesn't work
+	port = parseInt(process.argv[2]) || 80,
+	less = require('gulp-less'),
+	path = require('path');  // not sure why this doesn't work
 
 gulp.task('server', function(next){
 	var server = express().use(express.static( __dirname + '/public' )).listen(port, next);
@@ -11,11 +13,28 @@ gulp.task('server', function(next){
 	open("http://localhost" + portStr, "chrome");
 });
 
-gulp.task('default', ['server'], function(){
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', ['server', 'less'], function(){
 	var refresh = livereload();
+	console.log('watching');
 
 	gulp.watch(['public/**']).on('change', function(file){
 		refresh.changed(file.path);
 	});
 
+	gulp.watch('public/less/**/*.less').on('change', function(file){
+		console.log('less changed', file);
+		return gulp.src(file.path)
+			.pipe(less({
+				paths: ['public/less']
+			}))
+			.pipe(gulp.dest('./public/css'));
+	});
 });
