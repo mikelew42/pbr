4431e68424625701895ece7229f55eac14d19a6f
Michael <mike@lew42.com>
2016-01-29 13:25:26 -0600

    cleaned up base

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,8 @@
 ;(function(){
 	
-	strings = {};
-
 
 $(function(){
-	var $controls = $("<div>").addClass('controls').appendTo('body');
-	var $label = $("<input>").appendTo($controls);
-	var $create = $("<button>").html('add').appendTo($controls).click(function(){
-		var newStr = str.clone({ name: $label.val() });
-		strings[$label.val()] = newStr;
-		$label.val('');
-	});
 
-	myStr = str.clone({
-		name: 'myStr'
-	});
-	myStr.add('one', 'one');
-	myStr.add('two', '2');
-	myStr.save();
 });
 
-	// Test1 = HTMLElement.clone({
-	// 	name: "Test1"
-	// });
-
-	// Test1.addClass('test1');
 })();
